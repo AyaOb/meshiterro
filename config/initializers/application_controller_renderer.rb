8343932fb762cf71ece57554484f7e6f5dc9f797
AyaOb <Skilpro2162@gmail.com>
2021-07-18 05:18:51 +0000

    [Finish] 4-3 : add action:index,show,destroy & View:index,show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a05382fa87cf1388f2a67716496d9148434501471f5ae089163ce0d7d93c332a1377b8bac6e13511fc216cce81d08a0ea870277aafd0f444479ddafb362e55b8'
